3a2f308ae540d877f4365903501c3aa91bbaffa4
Mauro Nonis <mauro.nonis@gmail.com>
2023-04-04 17:27:50 +0200

    new project vue3

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,11 @@
 <template>
-  <v-app class="custom-font">
-    <v-app-bar color="transparent" short app absolute flat>
-      <v-icon
-        v-if="currentRouteName != 'Home'"
-        v-on:click="backToHome"
-        class="me-2"
-      >
-        mdi-chevron-left
-      </v-icon>
-      <span>Elder Dragon Highlander (Beta {{ version }})</span>
-      <v-spacer />
-      <v-avatar size="32">
-        <a class="link" href="https://gatherer.wizards.com/" target="_blank">
-          <v-icon color="primary">mdi-book-open-variant</v-icon>
-        </a>
-      </v-avatar>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <router-view />
 </template>
 
-<style>
-body {
-  background-color: #121212;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-.custom-font {
-  font-family: Futura, sans-serif !important;
-  color: var(--v-primary-base) !important;
-}
-
-.link {
-  text-decoration: none;
-}
-</style>
-
-<script>
-export default {
-  name: "App",
-
-  computed: {
-    version() {
-      return process.env.VUE_APP_VERSION;
-    },
-  },
-
-  methods: {
-    backToHome() {
-      this.$router.back();
-    },
-  },
-};
+export default defineComponent({
+  name: 'App'
+});
 </script>
